015ae304a725b751bcf035780883fe384216e588
MinhTu <minhtunguyenphan@gmail.com>
2020-01-09 15:45:00 +0700

    feat: facebook login

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -2,4 +2,3 @@ export * from './ping.controller';
 export * from './user.controller';
 export * from './admin.controller';
 export * from './permission.controller';
-export * from './auth.controller';
--- src/var.ts
+++ src/var.ts
@@ -1,6 +1,6 @@
 const VARIABLE = {
   ROUNDS: 10,
-  FACEBOOK_APP_ID: '',
+  FACEBOOK_APP_ID: '587524138705412',
   FACEBOOK_APP_SECRET: '',
   FACEBOOK_CALLBACK: 'http://localhost:3000/auth/facebook/callback',
 };
